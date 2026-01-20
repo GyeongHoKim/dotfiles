53e7bc683d7d78c5185dacbf59e73af6130dd128
GyeongHo Kim <gyeongho.dev@proton.me>
2026-01-20 14:00:01 +0900

    feat: default shell added

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,13 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+local shell
+if vim.fn.has("win32") == 1 then
+  shell = "pwsh"
+elseif vim.fn.has("unix") == 1 then
+  shell = "zsh"
+end
+
+if shell and vim.fn.executable(shell) == 1 then
+  vim.o.shell = shell
+end
